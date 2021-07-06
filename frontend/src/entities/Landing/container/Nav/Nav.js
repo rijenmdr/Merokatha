e6d23c68543d1566874698d25f0b5d9692e6d248
Rijen <rijmdr@gmail.com>
2021-07-06 18:28:40 +0545

    Fix the search bar in the navbar

--- frontend/src/entities/Landing/container/Nav/Nav.js
+++ frontend/src/entities/Landing/container/Nav/Nav.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
-const Nav=()=> {
-    const [searchBarVisible,setSearchBarVisible] = useState(false)
-    const [scrolled , setScrolled] = useState(false)
-    
+const Nav = () => {
+    const [searchBarVisible, setSearchBarVisible] = useState(false)
+    const [scrolled, setScrolled] = useState(false)
+
     const navRef = useRef()
     const inputRef = useRef()
-    
+
     navRef.current = scrolled
 
     useEffect(() => {
@@ -26,38 +26,43 @@ const Nav=()=> {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    const searchClickHandler = () =>{
+    const searchClickHandler = () => {
         setSearchBarVisible(true)
     }
 
     useEffect(() => {
-       if(searchBarVisible){
-        inputRef.current.focus()
-       }
+        if (searchBarVisible) {
+            inputRef.current.focus()
+        }
     }, [searchBarVisible])
 
     return (
-        <div className={`nav ${searchBarVisible && 'd-flex align-center'} ${scrolled ? 'scrolled' :''}`}>
-            { !searchBarVisible ? 
+        <div className={`nav ${searchBarVisible && 'd-flex align-center'} ${scrolled ? 'scrolled' : ''}`}>
             <div className={`d-flex justify-between align-center full-height`}>
                 <div className="logo">Merokatha</div>
-                <div className="nav-item">
-                    <li><NavLink className="item" to="/" activeClassName="active" exact>Home</NavLink></li>
-                    <li><NavLink className="item" to="/about" activeClassName="active">About</NavLink></li>
-                    <li><NavLink className="item" to="/contact" activeClassName="active">Contact</NavLink></li>
-                    <li className="dropdown"><a className="item" activeClassName="active">Account</a>
-                        <div className="dropdown-content">
-                            <NavLink className="sub-item" to="/login" activeClassName="">Login</NavLink>
-                            <NavLink className="sub-item" to="/signup" activeClassName="">Sign up</NavLink>
-                        </div>
-                    </li>
-                    <i onClick={searchClickHandler} className="material-icons-outlined">search</i>
-                </div>
-            </div> :
-            <div className={`search-bar`}>
-                <input ref={inputRef} type="text" placeholder="Search..."/>
-                <span className="material-icons-outlined" onClick={()=>setSearchBarVisible(false)}>close</span>
+                {!searchBarVisible &&
+                    <div className={`nav-item`}>
+                        <li><NavLink className="item" to="/" activeClassName="active" exact>Home</NavLink></li>
+                        <li><NavLink className="item" to="/about" activeClassName="active">About</NavLink></li>
+                        <li><NavLink className="item" to="/contact" activeClassName="active">Contact</NavLink></li>
+                        <li className="dropdown"><a className="item" activeClassName="active">Account</a>
+                            <div className="dropdown-content">
+                                <NavLink className="sub-item" to="/login" activeClassName="">Login</NavLink>
+                                <NavLink className="sub-item" to="/signup" activeClassName="">Sign up</NavLink>
+                            </div>
+                        </li>
+                        <i onClick={searchClickHandler} className="material-icons-outlined">search</i>
+                        <i className="material-icons-outlined menu d-none">
+                            menu
+                        </i>
+                    </div>
+                }
             </div>
+            {searchBarVisible &&
+                <div className={`search-bar`}>
+                    <input ref={inputRef} type="text" placeholder="Search..." />
+                    <span className="material-icons-outlined" onClick={() => setSearchBarVisible(false)}>close</span>
+                </div>
             }
         </div>
     )
